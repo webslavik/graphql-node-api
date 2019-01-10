cd397ed873ffc760f55bd3c7c01b28cddfe0dc5a
Slavik <slavik.sukhanov@iskyweb.com>
2019-01-10 21:53:03 +0200

    feat: add form for auth, CORS for api

--- app.js
+++ app.js
@@ -13,6 +13,21 @@ const { isAuth } = require('./middleware');
 app.use(bodyParser.json());
 app.use(isAuth);
 
+// CORS
+app.use((request, response, next) => {
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    if (request.method === 'OPTIONS') {
+        response.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return  response.status(200).json({});
+    }
+
+    next();
+});
+
 
 app.use('/api', graphqlHTTP({
     schema: graphQlSchema,
@@ -24,7 +39,7 @@ mongoose
     .connect('mongodb://localhost:27017/graphqlAPI', { useNewUrlParser: true })
     .then(() => {
         console.log('Connection to DB is successful.')
-        app.listen(3000);
+        app.listen(3001);
     })
     .catch((error) => {
         console.log(`Can't connect to DB: ${error}`)
--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -2,8 +2,110 @@ import React, { Component } from 'react';
 
 
 class AuthPage extends Component {
+    state = {
+        isLogin: true,
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.emailEl = React.createRef();
+        this.passwordEl = React.createRef();
+    }
+
+    switchModeHandler = () => {
+        this.setState(prevState => {
+            return {
+                isLogin: !prevState.isLogin,
+            }
+        });
+    }
+
+    submitHandler = event => {
+        event.preventDefault();
+
+        const email = this.emailEl.current.value;
+        const password = this.passwordEl.current.value;
+
+        if (email.trim().length === 0 ||
+            password.trim().length === 0) {
+            return;
+        }
+
+        let requestBody = {
+            query: `
+                query {
+                    login(email: "${email}", password: "${password}") {
+                        userId
+                        token
+                        tokenExpiration
+                    }
+                }
+            `
+        };
+
+        if (!this.state.isLogin) {
+            requestBody = {
+                query: `
+                    mutation {
+                        createUser(userInput: {email: "${email}", password: "${password}"}) {
+                            _id
+                            email
+                        }
+                    }
+                `
+            };
+        }
+
+
+        fetch('http://localhost:3001/api', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then(response => {
+            if (response.status !== 200 &&
+                response.status !== 201) {
+                throw new Error('Failed!');
+            }
+
+            return response.json();
+        })
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => console.log(error));
+            
+    }
+
     render() {
-        return <h1>Auth page</h1>;
+        return (
+            <div className='container mb-5'>
+                <div className='row justify-content-center pt-5'>
+                    <div className='col-sm-4'>
+                        <h1 className='mb-4'>
+                            {this.state.isLogin ? 'Sign in' : 'Sign up'}
+                        </h1>
+                        <form onSubmit={this.submitHandler}>
+                            <div className="form-group">
+                                <label htmlFor="email">Email address:</label>
+                                <input type="text" className="form-control" id="email" ref={this.emailEl} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="pwd">Password:</label>
+                                <input type="password" className="form-control" id="pwd" ref={this.passwordEl} />
+                            </div>
+                            <button type="submit" className="btn btn-primary mr-3">Submit</button>
+                            <button type="button" className="btn" onClick={this.switchModeHandler}>
+                                Switch to {this.state.isLogin ? 'Sign up' : 'Sign in'}
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
 
